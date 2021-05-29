77c2ebc217c77063d4f0534d196ea786a1b7d5e9
Onuoha Obinna <onuohasilver9@gmail.com>
2021-05-29 03:49:30 +0100

    baseFeatures

--- lib/src/screens/pages/landing.dart
+++ lib/src/screens/pages/landing.dart
@@ -11,37 +11,71 @@ class Landing extends StatelessWidget {
         height: size.h,
         width: size.w,
         color: Colors.black,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
+        child: Stack(
           children: [
-            Container(
-              height: size.h * .3,
-              width: size.w * .15,
-              decoration: BoxDecoration(
-                color: Colors.white.withOpacity(.2),
+            Positioned.fill(
+              top: size.h * .1,
+              child: Align(
+                alignment: Alignment.topCenter,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Material(
+                      child: InkWell(
+                        onTap: () {},
+                        child: Text('Resume',
+                            style: AppFonts(size).subtitle(
+                              0.01,
+                            )),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-            SizedBox(height: size.h * .04),
-            Text(
-              'Hello,',
-              style: AppFonts(size).subtitle(0.03, color: Colors.white70),
-            ),
-            SizedBox(height: size.h * .04),
-            Wrap(
-              children: [
-                Text(
-                  'I am ',
-                  style: AppFonts(size).subtitle(
-                    0.045,
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    height: size.h * .3,
+                    width: size.w * .15,
+                    decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(.2),
+                        borderRadius: BorderRadius.circular(30)),
                   ),
-                ),
-                Text(
-                  ' Onuoha Obinna.',
-                  style: AppFonts(size)
-                      .subtitle(0.045, fontWeight: FontWeight.bold),
-                ),
-              ],
+                  SizedBox(height: size.h * .04),
+                  Text(
+                    ' Hey,',
+                    style:
+                        AppFonts(size).subtitle(0.022, color: Colors.white54),
+                  ),
+                  SizedBox(height: size.h * .04),
+                  Wrap(
+                    alignment: WrapAlignment.end,
+                    crossAxisAlignment: WrapCrossAlignment.end,
+                    children: [
+                      Text(
+                        'I am ',
+                        style: AppFonts(size)
+                            .subtitle(0.026, color: Colors.white54),
+                      ),
+                      Text(
+                        ' Onuoha Obinna.',
+                        style: AppFonts(size)
+                            .subtitle(0.031, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: size.h * .014,
+                  ),
+                  Text('I build Software Solutions using Flutter.',
+                      style:
+                          AppFonts(size).subtitle(0.01, color: Colors.white54))
+                ],
+              ),
             ),
           ],
         ),
